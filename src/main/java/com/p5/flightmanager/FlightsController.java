d8d8589379b3201486064e21e2ad88cc0eacb791
Pipos Bogdan-Nicolae <bogdan.nicolae.pipos@gmail.com>
2019-07-10 16:42:12 +0300

    Get all flights completed

---
+++ src/main/java/com/p5/flightmanager/FlightsController.java
@@ -0,0 +1,30 @@
+package com.p5.flightmanager;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/flights")
+@Consumes("application/json")
+@Produces("application/json")
+@Transactional
+public class FlightsController {
+
+    @Resource
+    private FlightsRepository flightsRepository;
+
+    @GetMapping
+    ResponseEntity<Iterable<Flight>> getAll(){
+        return ResponseEntity.ok(flightsRepository.findAll());
+    }
+
+}

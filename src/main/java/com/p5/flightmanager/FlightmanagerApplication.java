e7a488fbaa168a223d01caf8a45f15b467c5fcd6
Bogdan B <speedy4all@gmail.com>
2019-07-10 13:50:55 +0300

    initial commit

---
+++ src/main/java/com/p5/flightmanager/FlightmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.p5.flightmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/p5/flightmanager/FlightmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.p5.flightmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightmanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

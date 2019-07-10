145024d879eda2e205a5952f510714043525c219
Buzila Alexandra <alybu@yahoo.com>
2019-07-10 16:41:37 +0300

    Enable transaction manager.

--- src/main/java/com/p5/flightmanager/FlightmanagerApplication.java
+++ src/main/java/com/p5/flightmanager/FlightmanagerApplication.java
@@ -2,13 +2,18 @@ package com.p5.flightmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EntityScan(basePackages = "com.p5.flightmanager") //in ce director va scana
+@EnableJpaRepositories(basePackages = "com.p5.flightmanager") //pentru a putea accesa datele
 public class FlightmanagerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(FlightmanagerApplication.class, args);
     }
 

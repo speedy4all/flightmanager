3fa7445893c5277a5d7dd70a0239cf3d8386cc25
Apinzarasoaie Alin <alinapinzarasoaie@gmail.com>
2019-07-10 15:12:23 +0300

    enable transaction manager

--- src/main/java/com/p5/flightmanager/FlightmanagerApplication.java
+++ src/main/java/com/p5/flightmanager/FlightmanagerApplication.java
@@ -2,8 +2,10 @@ package com.p5.flightmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class FlightmanagerApplication {
 
     public static void main(String[] args) {

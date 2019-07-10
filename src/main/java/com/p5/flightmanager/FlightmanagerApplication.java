08d12211f5cb1be3e5196da64502c34507ed456f
Bonciu Catalin <catalinbonciu@yahoo.com>
2019-07-10 16:42:01 +0300

    get all flights completed

--- src/main/java/com/p5/flightmanager/FlightmanagerApplication.java
+++ src/main/java/com/p5/flightmanager/FlightmanagerApplication.java
@@ -2,10 +2,15 @@ package com.p5.flightmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EntityScan(basePackages = "com.p5.flightmanager")
+@EnableJpaRepositories(basePackages = "com.p5.flightmanager")
+
 public class FlightmanagerApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/p5/flightmanager/FlightsController.java
@@ -0,0 +1,34 @@
+package com.p5.flightmanager;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.xml.ws.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/flights")
+@Consumes("application/json")
+@Produces("application/json")
+@Transactional
+public class FlightsController {
+
+
+
+    @Resource
+    private FlightsRepository flightsRepository;
+
+    @GetMapping
+    ResponseEntity<Iterable<Flight>>getAll() {
+        return ResponseEntity.ok(flightsRepository.findAll());
+    }
+
+}

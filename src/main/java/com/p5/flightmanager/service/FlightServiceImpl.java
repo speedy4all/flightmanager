3f31294be179370183f4cb09826111d43f321dc5
Bogdan B <speedy4all@gmail.com>
2019-07-11 16:57:14 +0300

    - adapter - body params for creation

--- src/main/java/com/p5/flightmanager/service/FlightServiceImpl.java
+++ src/main/java/com/p5/flightmanager/service/FlightServiceImpl.java
@@ -3,11 +3,12 @@ package com.p5.flightmanager.service;
 import com.p5.flightmanager.repository.models.Flight;
 import com.p5.flightmanager.repository.FlightsRepository;
 import com.p5.flightmanager.service.api.FlightService;
+import com.p5.flightmanager.service.dto.FlightAdapter;
+import com.p5.flightmanager.service.dto.FlightDto;
+import com.p5.flightmanager.service.exceptions.NoFlightException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -20,26 +21,30 @@ public class FlightServiceImpl implements FlightService {
     private FlightsRepository flightsRepository;
 
 
-    public List<Flight> getAll() {
+    public List<FlightDto> getAll() {
 
-        List<Flight> flights = new ArrayList<>();
-        flightsRepository.findAll().forEach(flights::add);
-        return flights;
+        return FlightAdapter.toListDto(flightsRepository.findAll());
     }
 
     @Override
-    public Flight createFlight() {
+    public FlightDto createFlight(FlightDto flightDto) {
+
+
         Flight newFlight = new Flight("First flight", "BUH", "CN", 8d, new Date(), new Date());
+        Flight flight = flightsRepository.save(newFlight);
 
-        return flightsRepository.save(newFlight);
+        return FlightAdapter.toDto(flight);
     }
 
+
+
     @Override
-    public Flight getById(String id) {
+    public FlightDto getById(String id) {
         Optional<Flight> optionalFlight = flightsRepository.findById(UUID.fromString(id));
         if(optionalFlight.isPresent()) {
-            return optionalFlight.get();
+            Flight flight = optionalFlight.get();
+            return FlightAdapter.toDto(flight);
         }
-        return null;
+        throw new NoFlightException();
     }
 }

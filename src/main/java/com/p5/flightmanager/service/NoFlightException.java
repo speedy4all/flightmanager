54108aff52671e71134bb359155355dd42142f86
Biolan Andrei <andrew.rld4@gmail.com>
2019-07-11 16:42:56 +0300

    hide some flight informations, added Dto

---
+++ src/main/java/com/p5/flightmanager/service/NoFlightException.java
@@ -0,0 +1,12 @@
+package com.p5.flightmanager.service;
+
+public class NoFlightException extends RuntimeException {
+    public NoFlightException() {
+        
+    }
+
+    @Override
+    public String getMessage() {
+        return "No flight found.";
+    }
+}
